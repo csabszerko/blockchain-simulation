878d9bdc67ac279e37669945ed67a4e872741a6d
csabszerko <kiss.csaba.david@gmail.com>
2025-09-11 20:46:57 +0200

    fix(ts rewrite): change Block back to a class

--- core/block.ts
+++ core/block.ts
@@ -1,10 +1,26 @@
 import type Transaction from "./transaction.js";
 
-export default interface Block {
+export default class Block {
   index: number;
   timestamp: number;
   transactions: Transaction[];
   previousHash: string;
   hash: string;
   nonce: number;
+
+  constructor(
+    index: number,
+    timestamp: number,
+    transactions: Transaction[],
+    previousHash: string,
+    hash: string,
+    nonce: number
+  ) {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.transactions = transactions;
+    this.previousHash = previousHash;
+    this.hash = hash;
+    this.nonce = nonce;
+  }
 }
