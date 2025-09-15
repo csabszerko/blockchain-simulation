5b841f46deffbf4c83204f7f59100bb33f121e06
csabszerko <kiss.csaba.david@gmail.com>
2025-09-15 22:34:27 +0200

    fix: utxo table overflow on mobile

--- ui/src/components/custom/add_transaction/UtxoListContainer.tsx
+++ ui/src/components/custom/add_transaction/UtxoListContainer.tsx
@@ -14,11 +14,11 @@ export default function UtxoListContainer({ utxos }: { utxos: UTXOSet }) {
     <Table className="table-fixed w-full ">
       <TableHeader className="table-header-group">
         <TableRow>
-          <TableHead className="w-4/5 md:w-4/7 truncate">txid</TableHead>
+          <TableHead className="w-2/5 md:w-4/7 truncate">txid</TableHead>
           <TableHead className="w-1/5 md:w-1/7 !text-center truncate">
             vout
           </TableHead>
-          <TableHead className="w-1/5 md:w-2/7 !text-center truncate">
+          <TableHead className="w-2/5 md:w-2/7 !text-center truncate">
             amount
           </TableHead>
         </TableRow>
